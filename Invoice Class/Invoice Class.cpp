c00bcf0f80ab4086dbfe6c04d53242f623de6b82
hcharise <110205350+hcharise@users.noreply.github.com>
2024-01-24 19:31:28 -0700

    - Built constructor  - Filled body of getters and setters  - Added main  - Create instance of invoice with intialization

--- Invoice Class/Invoice Class.cpp
+++ Invoice Class/Invoice Class.cpp
@@ -10,8 +10,6 @@ HW #1 - Exercise 3.10 p100
 This program does so on and so forth...
 
 
-
-
 3.10: Create a class called Invoice that a hardware store might use to represent
 an invoice for an item sold at the store. An Invoice should include four data members:
  - a part number (string)
@@ -28,29 +26,62 @@ Write a test program that demonstrates class Invoice's capabilities.
 
 #include <string>
 
+using std::string;
+
+
 class Invoice {
 public:
-	getPartNumber{
+	// CONSTRUCTOR
+	
+	Invoice(string partNum, string partDesc, int itemQuant, int itemPrice)
+	: partNumber{partNum}, partDescription{partDesc}, itemQuantity{itemQuant}, pricePerItem{itemPrice} {
+		// empty body
+	} // end Invoice constructor
 
+
+	// SETTERS
+	string setPartNumber(string partNum) {
+		partNumber = partNum;
+	} // end setPartNumber
+
+	string setPartDescription(string partDesc) {
+		partDescription = partDesc;
+	} // end setPartDescription
+
+	int setItemQuantity(int itemQuant) {
+		itemQuantity = itemQuant;
+	} // end setItemQuantity
+
+	int setPricePerItem(int itemPrice) {
+		pricePerItem = itemPrice;
+	} // end setPricePerItem
+
+	// GETTERS
+	string getPartNumber() const {
+		return partNumber;
 	} // end getPartNumber
 
-	getPartDescription{
-
+	string getPartDescription() const {
+		return partDescription;
 	} // end getPartDescription
 
-	getItemQuantity{
-
+	int getItemQuantity() const {
+		return itemQuantity;
 	} // end getItemQuantity
 
-	getPricePerItem{
-
+	int getPricePerItem() const {
+		return pricePerItem;
 	} // end getPricePerItem
 
-
 private:
-	std::string partNumber;
-	std::string partDescription;
+	string partNumber;
+	string partDescription;
 	int itemQuantity;
 	int pricePerItem;
 }; // end class Invoice
 
+// MAIN
+int main() {
+	Invoice drawerPullInvoice{ "P123", "Drawer Pull, Black", 12, 3 };
+
+} // end main
